bcf21356e67e2d0095e3cea7750a0090bea33adb
NoName <duran.reddy13@gmail.com>
2022-05-03 23:55:29 +0200

    Added moon and lighting to scene

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import * as CANNON from '../lib/cannon-es.js'
 import Player from '../src/player.js'
 import { Guy } from './guy.js';
 import { FirstPersonCamera } from './FirstPersonControls.js'
+import { pointLightCreator, InteriorWallLightCreator, ChandelierCreator, BedroomLightCreator, moonCreator, addSphereMoon } from './lights.js';
+
 
 class Ground extends THREE.Group{
   constructor(scene, world){
@@ -21,7 +23,7 @@ class Ground extends THREE.Group{
     let aoColor = textLoader.load("./textures/forrest_ground_01_rough_ao_1k.jpg");
 
     const ground = new THREE.Mesh(new THREE.PlaneGeometry(30, 30, 512, 512), 
-      new THREE.MeshStandardMaterial({
+      new THREE.MeshLambertMaterial({
         map: baseColor,
         normalMap: normalColor,
         displacementMap: heightColor,
@@ -39,6 +41,8 @@ class Ground extends THREE.Group{
     })
 
     this.body.quaternion.setFromEuler(-Math.PI / 2, 0, 0);
+    ground.receiveShadow = true;
+    ground.castShadow = true;
 
     this.body.position.y = -1
     
@@ -77,15 +81,25 @@ var init = function(){
   
   var renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight,);
+  renderer.shadowMap.enabled = true;
   
   document.body.appendChild(renderer.domElement);
 
   var cube;
   const loader = new THREE.GLTFLoader();
-  loader.load('../res/meshes/FirstFloor.glb', function(gltf){
+  loader.load('../res/meshes/House.glb', function(gltf){
     cube = gltf.scene
     cube.position.set(5,-0.83,-4);
     cube.scale.set(1, 1, 1);
+
+        //Creating shadows for each child mesh
+        gltf.scene.traverse(function(node){
+          if(node.type === 'Mesh'){     
+              node.castShadow=true;
+              node.receiveShadow=true; //allows us to put shadows onto the walls
+          }
+      });
+
     scene.add(cube);
   }, (xhr) => {
     console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
@@ -93,12 +107,26 @@ var init = function(){
     console.log(error);
   });
 
+  //Adds the interior wall lights
+  //InteriorWallLightCreator(0xFFFFFF,0.5,50,1,scene,[0,2,0],[1,1,1],[0,0,0])
+  //ChandelierCreator(0xFFFFFF,0.1,50,1,scene,[0,2,0],[1,1,1],[0,0,0])
+  //BedroomLightCreator(0xFFFFFF,0.1,25,1,scene,[0,1.75,10],[1,1,1],[0,0,0])
+  
+  //Setting up the moon
+  var moonLight = moonCreator(0xFFFFFF,0.8,10000,1)
+  scene.add(moonLight);
+  var moonSphere = addSphereMoon(2);
+  scene.add(moonSphere)
+  
+  //Added skybox
   const skybox = sky()
   scene.add(skybox)
+
   const initial_position = new CANNON.Vec3(0, 0, 5)
   const guy = new Player(scene, world, camera)
   const fpCamera = new FirstPersonCamera(camera);
   const light = new THREE.AmbientLight();
+  light.intensity=0.2;
   scene.add(light);
 
   const timestep = 1/60
@@ -110,6 +138,7 @@ var init = function(){
 
   var delta = 0
   var time = new Date().getTime()
+  var speed= 0
 
   var update = function(){//game logic
     //stats.begin()
@@ -118,6 +147,19 @@ var init = function(){
     time = new_time
     guy.update(delta)
     g.update()
+
+    //Rotates the skybox
+    skybox.rotation.x+=0.0005;
+    skybox.rotation.y+=0.0005;
+    skybox.rotation.z+=0.0005;
+
+    //Move the moon
+    speed+=0.001
+    moonLight.position.y = 20*(Math.sin(speed))+10;
+    moonLight.position.z = 10*(Math.cos(speed));
+    moonSphere.position.y = 20*(Math.sin(speed))+10;
+    moonSphere.position.z = 10*(Math.cos(speed));
+
     world.step(timestep)
     fpCamera.update();
     //stats.end()
