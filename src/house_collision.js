3ec099a36a9166f896bc16744170a9b45f1b75e2
NoName <duran.reddy13@gmail.com>
2022-05-05 23:41:20 +0200

    Added staircase

--- src/house_collision.js
+++ src/house_collision.js
@@ -21,19 +21,21 @@ function makeHouse(scene,world){
     console.log(error);
   });
 
-  //foundation
-  makeCollisionCube(scene,world,[24.7,1.2,20],[0,-0.4,-3.9],[0,0,0]);
+//   //foundation
+//   makeCollisionCube(scene,world,[24.7,1.2,20],[0,-0.4,-3.9],[0,0,0]);
 
-  //entrance steps
-  makeCollisionCube(scene,world,[5,0.75,0.6],[0,-0.6,6.8],[0,0,0]);
-  makeCollisionCube(scene,world,[5,0.5,0.6],[0,-0,6.25],[0,0,0]);
+//   //entrance steps
+//   makeCollisionCube(scene,world,[5,0.75,0.6],[0,-0.6,6.8],[0,0,0]);
+//   makeCollisionCube(scene,world,[5,0.5,0.6],[0,-0,6.25],[0,0,0]);
 
-  //walls
-  makeCollisionCube(scene,world,[16,13,0.4],[-12.3,6.5,-6],[0,Math.PI/2,0]); //right
-  makeCollisionCube(scene,world,[16,13,0.4],[12.3,6.5,-6],[0,Math.PI/2,0]); //left
-  makeCollisionCube(scene,world,[10,25,0.4],[0,5,-13.95],[0,0,Math.PI/2]); //back
-  //makeCollisionCube(scene,world,[10,25,0.4],[0,5,1.95],[0,0,Math.PI/2]); //front
+//   //walls
+//   makeCollisionCube(scene,world,[16,13,0.4],[-12.3,6.5,-6],[0,Math.PI/2,0]); //right
+//   makeCollisionCube(scene,world,[16,13,0.4],[12.3,6.5,-6],[0,Math.PI/2,0]); //left
+//   makeCollisionCube(scene,world,[10,25,0.4],[0,5,-13.95],[0,0,Math.PI/2]); //back
+//   makeCollisionCube(scene,world,[10,25,0.4],[0,5,1.95],[0,0,Math.PI/2]); //front
 
+  //inner staircase
+  makeCollisionStaircase(scene,world,[5,0.45,0.5],[0,0.4,-8.5],[0,0,0]);
 
 }
 
@@ -60,6 +62,15 @@ function makeCollisionCube(scene,world,boxGeoSize,boxPos,rotationArr){
     boxBody.position.copy(box.position); //Copy position
     boxBody.quaternion.copy(box.quaternion); //Copy orientation
 
+}
+
+function makeCollisionStaircase(scene,world,boxGeoSize,boxPos,rotationArr){
+    for(var i =0;i<5;i++){
+        makeCollisionCube(scene,world,boxGeoSize,boxPos,rotationArr)
+        boxPos[1]+=0.5;
+        boxPos[2]-=0.5;
+    }
+    
 }
 
 export {makeHouse}
