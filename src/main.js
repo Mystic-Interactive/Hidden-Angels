bfedfa14cd6f5084b638e4765486a5e923d8e87d
Liam Carstens <lordinfernus09@gmail.com>
2022-06-04 12:12:31 +0200

    Cleaning Code

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { sky } from './sky.js';
+import { Ground } from './ground.js';
 import * as CANNON from '../lib/cannon-es.js'
 import Player from '../src/player.js'
 import Monster from '../src/monster.js'
@@ -10,6 +11,18 @@ import {HUD, tookDamage,changeInventorySelected} from './overlay.js'
 import {makeFirstFloor,makeSecondFloor,makeBasement,makeFourthFloor,removeFloor} from './house_collision.js'
 import { Reflector } from '../lib/Reflector.js'
 
+var  camera;
+var scene;
+var renderer;
+var world;
+
+const timestep = 1/60
+var delta = 0
+var time = new Date().getTime()
+var speed = 0
+var t = 41;
+var selected = 0;
+
 var paused = false;
 var curr_lvl = null;
 var lvl = null;
@@ -17,97 +30,60 @@ var lvl1_uuid = "";
 var lvl2_uuid = "";
 var lvl3_uuid = "";
 var lvl4_uuid = "";
+var sceneHUD;
+var cameraHUD;
+var mousePos;
 
+var player;
+var monster;
+var ground;
+var moonLight;
+var moonSphere;
+var torchLight
 
-
-// Class to make the world's surface 
-
-class Ground extends THREE.Group{
-  // Constructor to get scene and camera and place plane in world
-  constructor(scene, world){
-    super();
-    this.scene = scene;
-    this.world = world;
-    this.define()
-  }
-  
-  define(){ // Create plane with ground textures and add it to world
-    const textLoader = new THREE.TextureLoader();
-    let baseColor = textLoader.load("./textures/forrest_ground_01_diff_1k.jpg");
-    let aoColor = textLoader.load("./textures/forrest_ground_01_rough_ao_1k.jpg");
-
-    // Create plane with ground textures
-    const ground = new THREE.Mesh(new THREE.PlaneGeometry(30, 30), 
-      new THREE.MeshLambertMaterial({ // Lambert Material used so shadows look smooth
-        map: baseColor,
-        aoMap: aoColor,
-      })
-    );
-    ground.geometry.attributes.uv2 = ground.geometry.attributes.uv;
-
-    this.body = new CANNON.Body({ // create physics body for plane
-      shape: new CANNON.Box(new CANNON.Vec3(60, 60, 0.1)), // Cannon.js planes are infinite so use a cube instead
-      type: CANNON.Body.STATIC,// Isn't affected by gravity
-      material: new CANNON.Material()
-    })
-
-    // Enable shadows on surface
-    this.body.quaternion.setFromEuler(-Math.PI / 2, 0, 0);
-    ground.receiveShadow = true;
-    ground.castShadow = true;
-
-    this.body.position.y = -1 
-    
-    // Add both body and plane to world
-    this.add(ground)
-    this.scene.add(this)
-    this.world.addBody(this.body)
-  }
-  
-  update(){ // link the physics body to the plane
-    try{
-      this.position.copy(this.body.position)
-      this.quaternion.copy(this.body.quaternion)
-    }catch{
-      // console.log("umm")
-    }
-    
-  }
-}
 
 // Initialization of game (world, level, HUD, etc.)
 var init = function(){
+  let world_canvas = document.getElementById('MainWorld');
   var hud_canvas = document.getElementById('myCanvas');
+  world_canvas.width = window.innerWidth;
+  world_canvas.height = window.innerHeight;
   hud_canvas.width = window.innerWidth;
   hud_canvas.height = window.innerHeight;
 
 
-  const world = new CANNON.World({
+  world = new CANNON.World({
     gravity: new CANNON.Vec3(0, -9.81, 0)
   })
 
-  var scene = new THREE.Scene();
+  scene = new THREE.Scene();
 	
-  var camera = new THREE.PerspectiveCamera(
+  camera = new THREE.PerspectiveCamera(
     95, // field of view (fov)
     window.innerWidth/window.innerHeight, // browser aspect ratio
     0.1, // near clipping plane
     1000 // far clipping plane
   );
   
-  var renderer = new THREE.WebGLRenderer({maxLights: 8});
+  renderer = new THREE.WebGLRenderer({
+    maxLights: 8,
+    canvas: world_canvas,
+  });
   renderer.setSize(window.innerWidth, window.innerHeight,);
   renderer.shadowMap.enabled = true;
-  renderer.autoClear=false;
+  renderer.autoClear = false;
   
   document.body.appendChild(renderer.domElement);
 
-  const mousePos = new THREE.Vector2();
+  mousePos = new THREE.Vector2();
+
+  //Create and add ground mesh to world
+  ground = new Ground(scene, world)
   
   //Setting up the moon
-  var moonLight = moonCreator(0xFFFFFF,0.8,10000,1,-0.0045);
+  moonLight = moonCreator(0xFFFFFF,0.8,10000,1,-0.0045);
   scene.add(moonLight);
-  var moonSphere = addSphereMoon(2);
+  moonSphere = addSphereMoon(2);
   scene.add(moonSphere)
   
   //Added skybox
@@ -116,200 +92,34 @@ var init = function(){
 
   const initial_position = new CANNON.Vec3(0, 1, 5)
 
-
-
-
   var path = [
     new THREE.Vector3(2, 0, 2), 
     new THREE.Vector3(2, 0, -2),
     new THREE.Vector3(-2, 0, -2),
     new THREE.Vector3(-2, 0, 2)
   ]
+
   const monsters = []
-  const player = new Player(scene, world, camera, initial_position, monsters)
-  const monster = new Monster(scene, world,new THREE.Vector3(1, 0, -6), path, player)
+  player = new Player(scene, world, camera, initial_position, monsters)
+  monster = new Monster(scene, world,new THREE.Vector3(1, 0, -6), path, player)
   monsters.push(monster)
- // const fpCamera = new FirstPersonCamera(camera);
+
   //var monster_v2 = new monster_ai(scene,player);
   const light = new THREE.AmbientLight();
   // light.intensity=0.02;
   light.intensity=1;
   scene.add(light);
 
-  const timestep = 1/60
+  const PointerLock = new PointerLockControls(camera,document.body);
+  const blocker = document.getElementById( 'myCanvas' );
+  blocker.addEventListener('click', function (){
+	  PointerLock.lock();
+	});
 
-  const g = new Ground(scene, world)
-
-  scene.add(g)
-
-  var delta = 0
-  var time = new Date().getTime()
-  var speed = 0
-
- const PointerLock = new PointerLockControls(camera,document.body);
- const blocker = document.getElementById( 'myCanvas' );
-	blocker.addEventListener( 'click', function () {
-		PointerLock.lock();
-	} );
-
-
-  //Creating the pause menu
-  var container = document.createElement('canvas');
-  container.setAttribute(
-    "style","width:1px; height:1px","position:absolute");
-    document.body.appendChild( container );
-    var cameraHUD = new THREE.OrthographicCamera(window.innerWidth / - 2, window.innerWidth / 2, window.innerHeight / 2,window.innerHeight / - 2, - 500, 1000 );
-      cameraHUD.position.x = 0;
-      cameraHUD.position.y = 0;
-      cameraHUD.position.z = 0;
-    
-    var sceneHUD = new THREE.Scene();
-
-    var spriteMaterial = new THREE.SpriteMaterial({map:
-      THREE.ImageUtils.loadTexture(
-      "../res/textures/pause_menu/level-1.png")});
-      var sprite = new THREE.Sprite(spriteMaterial);
-      sprite.position.set(0,window.innerHeight/3,0);
-      sprite.scale.set(window.innerHeight/1.75,window.innerWidth/10,1);
-
-      var spriteMaterial2 = new THREE.SpriteMaterial({map:
-        THREE.ImageUtils.loadTexture(
-        "../res/textures/pause_menu/level-2.png")});
-        var sprite2 = new THREE.Sprite(spriteMaterial2);
-        sprite2.position.set(0,window.innerHeight/12,0);
-        sprite2.scale.set(window.innerHeight/1.75,window.innerWidth/10,1);
-
-        var spriteMaterial3 = new THREE.SpriteMaterial({map:
-          THREE.ImageUtils.loadTexture(
-          "../res/textures/pause_menu/level-3.png")});
-          var sprite3 = new THREE.Sprite(spriteMaterial3);
-          sprite3.position.set(0,-window.innerHeight/6,0);
-          sprite3.scale.set(window.innerHeight/1.75,window.innerWidth/10,1);
-
-        var spriteMaterial4 = new THREE.SpriteMaterial({map:
-           THREE.ImageUtils.loadTexture(
-           "../res/textures/pause_menu/level-4.png")});
-           var sprite4 = new THREE.Sprite(spriteMaterial4);
-           sprite4.position.set(250,-window.innerHeight/8,0);
-           sprite4.scale.set(window.innerHeight/1.75,window.innerWidth/10,1);
+  createMenu()
       
-      lvl1_uuid = sprite.uuid;
-      lvl2_uuid = sprite2.uuid;
-      lvl3_uuid = sprite3.uuid;
-      lvl4_uuid = sprite4.uuid
-
-      sceneHUD.add(sprite);
-      sceneHUD.add(sprite2);
-      sceneHUD.add(sprite3);
-      sceneHUD.add(sprite4);
-
-
-var t =41;
-var selected = 0;
-      
-var torchLight = torch(0xFFFFFF,1,5,1,-0.004,[0,0,0])
-scene.add(torchLight)
-  var update = function(){//game logic
-    if(!paused){
-      monster.update( delta );
-      const new_time = new Date().getTime()
-      delta = new_time - time
-      time = new_time
-      player.update(delta)
-      //monster.update(delta)
-      g.update()
-
-      //Showing that we can decrease the visible hearts on the fly
-      const d = new Date();
-      // console.log(d.getMinutes())
-      if(d.getMinutes()==t){
-        selected+=2;
-        tookDamage(1.5);
-        changeInventorySelected(selected)
-        HUD(8,[1,2,3,4,5,6,7,-1]);
-        t+=1;
-      }
-      HUD(8,[1,2,3,4,5,6,7,-1]);
-
-      
-
-      //Move the moon and skybox only when you can see them to reduce the compuation needed
-      if(curr_lvl==4){
-        //Rotates and moves the moon
-        speed+=0.001
-        moonLight.position.y = 20*(Math.sin(speed))+50;
-        moonLight.position.z = 10*(Math.cos(speed));
-        moonSphere.position.y = 20*(Math.sin(speed))+50;
-        moonSphere.position.z = 10*(Math.cos(speed));
-        moonSphere.rotation.x+=0.005;
-        moonSphere.rotation.y+=0.005;
-        moonSphere.rotation.z+=0.005;
-        
-        //Rotates the skybox
-        skybox.rotation.x+=0.0005;
-        skybox.rotation.y+=0.0005;
-        skybox.rotation.z+=0.0005;
-      }
-      else{
-        moonLight.position.set(0,10,0);
-        moonSphere.position.set(0,10,0);
-      }
-      
-      
-
-      world.step(timestep)
-      // console.log(camera.position)
-      torchLight.position.set(player.position.x,player.position.y,player.position.z)
-
-      
-
-
-
-      //stats.end()
-    }
-    else{
-    const rayCaster = new THREE.Raycaster();
-    rayCaster.setFromCamera(mousePos,cameraHUD);
-    const intersects = rayCaster.intersectObjects(sceneHUD.children);
-    if(intersects.length==0){
-      console.log("Nothing selected")
-      lvl = null;
-    }
-    else if(intersects[0].object.uuid === lvl1_uuid){
-      console.log("Level 1 Highlighted")
-      lvl = 1;
-    }
-    else if(intersects[0].object.uuid === lvl2_uuid){
-      console.log("Level 2 Highlighted")
-      lvl = 2;
-    }
-    else if(intersects[0].object.uuid === lvl3_uuid){
-      console.log("Level 3 Highlighted")
-      lvl = 3;
-    }
-    else{
-      console.log("Level 4 Highlighted")
-      lvl = 4;
-    }
-
-    }
-    
-  };
-
-  var render = function(){//draw scene
-    renderer.render(scene, camera);
-    if(paused){
-      //renderer.clearDepth();
-      renderer.render(sceneHUD, cameraHUD); 
-    }
-    
-  };
-
-  var GameLoop = function(){//run game loop(update, render, repeat)
-    update();
-    render();
-    requestAnimationFrame(GameLoop);
-  };
+  torchLight = torch(0xFFFFFF,1,5,1,-0.004,[0,0,0])
+  scene.add(torchLight)
 
   window.addEventListener('resize', () => {
     hud_canvas.width = window.innerWidth;
@@ -452,6 +262,154 @@ scene.add(torchLight)
 
 
   GameLoop()
+};
+
+function update(){//game logic
+  if(!paused){
+    monster.update( delta );
+    const new_time = new Date().getTime()
+    delta = new_time - time
+    time = new_time
+    player.update(delta)
+    //monster.update(delta)
+    ground.update()
+
+    //Showing that we can decrease the visible hearts on the fly
+    const d = new Date();
+    // console.log(d.getMinutes())
+    if(d.getMinutes()==t){
+      selected+=2;
+      tookDamage(1.5);
+      changeInventorySelected(selected)
+      HUD(8,[1,2,3,4,5,6,7,-1]);
+      t+=1;
+    }
+    HUD(8,[1,2,3,4,5,6,7,-1]);
+
+
+    //Move the moon and skybox only when you can see them to reduce the compuation needed
+    if(curr_lvl==4){
+      //Rotates and moves the moon
+      speed+=0.001
+      moonLight.position.y = 20*(Math.sin(speed))+50;
+      moonLight.position.z = 10*(Math.cos(speed));
+      moonSphere.position.y = 20*(Math.sin(speed))+50;
+      moonSphere.position.z = 10*(Math.cos(speed));
+      moonSphere.rotation.x+=0.005;
+      moonSphere.rotation.y+=0.005;
+      moonSphere.rotation.z+=0.005;
+      
+      //Rotates the skybox
+      skybox.rotation.x+=0.0005;
+      skybox.rotation.y+=0.0005;
+      skybox.rotation.z+=0.0005;
+    }
+    else{
+      moonLight.position.set(0,10,0);
+      moonSphere.position.set(0,10,0);
+    }
+    
+    world.step(timestep)
+    // console.log(camera.position)
+    torchLight.position.set(player.position.x,player.position.y,player.position.z)
+  }
+  else{
+    const rayCaster = new THREE.Raycaster();
+    rayCaster.setFromCamera(mousePos,cameraHUD);
+    const intersects = rayCaster.intersectObjects(sceneHUD.children);
+    if(intersects.length==0){
+      console.log("Nothing selected")
+      lvl = null;
+    }
+    else if(intersects[0].object.uuid === lvl1_uuid){
+      console.log("Level 1 Highlighted")
+      lvl = 1;
+    }
+    else if(intersects[0].object.uuid === lvl2_uuid){
+      console.log("Level 2 Highlighted")
+      lvl = 2;
+    }
+    else if(intersects[0].object.uuid === lvl3_uuid){
+      console.log("Level 3 Highlighted")
+      lvl = 3;
+    }
+    else{
+      console.log("Level 4 Highlighted")
+      lvl = 4;
+    }
+  }
+};
+
+function render(){//draw scene
+  renderer.render(scene, camera);
+  if(paused){
+    //renderer.clearDepth();
+    renderer.render(sceneHUD, cameraHUD); 
+  }
+};
+
+function createMenu(){
+  //Creating the pause menu
+  var container = document.createElement('canvas');
+
+  container.setAttribute("style","width:1px; height:1px","position:absolute");
+  
+  document.body.appendChild( container );
+  cameraHUD = new THREE.OrthographicCamera(window.innerWidth / - 2, window.innerWidth / 2, window.innerHeight / 2,window.innerHeight / - 2, - 500, 1000 );
+  
+  cameraHUD.position.x = 0;
+  cameraHUD.position.y = 0;
+  cameraHUD.position.z = 0;
+  
+  sceneHUD = new THREE.Scene();
+
+  var spriteMaterial = new THREE.SpriteMaterial({
+    map: new THREE.TextureLoader().load("../res/textures/pause_menu/level-1.png")
+  });
+
+  var sprite = new THREE.Sprite(spriteMaterial);
+  sprite.position.set(0,window.innerHeight/3,0);
+  sprite.scale.set(window.innerHeight/1.75,window.innerWidth/10,1);
+
+  var spriteMaterial2 = new THREE.SpriteMaterial({
+    map: new THREE.TextureLoader().load("../res/textures/pause_menu/level-2.png")
+  });
+
+  var sprite2 = new THREE.Sprite(spriteMaterial2);
+  sprite2.position.set(0,window.innerHeight/12,0);
+  sprite2.scale.set(window.innerHeight/1.75,window.innerWidth/10,1);
+
+  var spriteMaterial3 = new THREE.SpriteMaterial({
+    map: new THREE.TextureLoader().load("../res/textures/pause_menu/level-3.png")
+  });
+
+  var sprite3 = new THREE.Sprite(spriteMaterial3);
+  sprite3.position.set(0,-window.innerHeight/6,0);
+  sprite3.scale.set(window.innerHeight/1.75,window.innerWidth/10,1);
+
+  var spriteMaterial4 = new THREE.SpriteMaterial({
+    map: new THREE.TextureLoader().load("../res/textures/pause_menu/level-4.png")
+  });
+
+  var sprite4 = new THREE.Sprite(spriteMaterial4);
+  sprite4.position.set(250,-window.innerHeight/8,0);
+  sprite4.scale.set(window.innerHeight/1.75,window.innerWidth/10,1);
+  
+  lvl1_uuid = sprite.uuid;
+  lvl2_uuid = sprite2.uuid;
+  lvl3_uuid = sprite3.uuid;
+  lvl4_uuid = sprite4.uuid
+
+  sceneHUD.add(sprite);
+  sceneHUD.add(sprite2);
+  sceneHUD.add(sprite3);
+  sceneHUD.add(sprite4);
+}
+
+function GameLoop(){//run game loop(update, render, repeat)
+  update();
+  render();
+  requestAnimationFrame(GameLoop);
 };
 
 window.init = init
