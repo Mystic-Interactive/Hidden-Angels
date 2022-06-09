69198e9df1aa1b0e63897ae6b630ea7f8a09cf8f
NoName-0512 <duran.reddy13@gmail.com>
2022-06-09 15:53:14 +0200

    new skybox

--- src/sky.js
+++ src/sky.js
@@ -4,13 +4,21 @@ function sky(){// function to make skybox
     skyBoxGeo = new THREE.BoxGeometry(500,500,500); //make a cube
     let materialArray = [];
 
+    // // Load all the tetxures to fill the cube's interior
+    // let texture_ft = new THREE.TextureLoader().load('../res/textures/skybox/kenon_star_ft.jpg');
+    // let texture_bk = new THREE.TextureLoader().load('../res/textures/skybox/kenon_star_bk.jpg');
+    // let texture_up = new THREE.TextureLoader().load('../res/textures/skybox/kenon_star_up.jpg');
+    // let texture_dn = new THREE.TextureLoader().load('../res/textures/skybox/kenon_star_dn.jpg');
+    // let texture_rt = new THREE.TextureLoader().load('../res/textures/skybox/kenon_star_rt.jpg');
+    // let texture_lf = new THREE.TextureLoader().load('../res/textures/skybox/kenon_star_lf.jpg');
+
     // Load all the tetxures to fill the cube's interior
-    let texture_ft = new THREE.TextureLoader().load('../res/textures/skybox/kenon_star_ft.jpg');
-    let texture_bk = new THREE.TextureLoader().load('../res/textures/skybox/kenon_star_bk.jpg');
-    let texture_up = new THREE.TextureLoader().load('../res/textures/skybox/kenon_star_up.jpg');
-    let texture_dn = new THREE.TextureLoader().load('../res/textures/skybox/kenon_star_dn.jpg');
-    let texture_rt = new THREE.TextureLoader().load('../res/textures/skybox/kenon_star_rt.jpg');
-    let texture_lf = new THREE.TextureLoader().load('../res/textures/skybox/kenon_star_lf.jpg');
+    let texture_ft = new THREE.TextureLoader().load('../res/textures/skybox_new/corona_ft.png');
+    let texture_bk = new THREE.TextureLoader().load('../res/textures/skybox_new/corona_bk.png');
+    let texture_up = new THREE.TextureLoader().load('../res/textures/skybox_new/corona_up.png');
+    let texture_dn = new THREE.TextureLoader().load('../res/textures/skybox_new/corona_dn.png');
+    let texture_rt = new THREE.TextureLoader().load('../res/textures/skybox_new/corona_rt.png');
+    let texture_lf = new THREE.TextureLoader().load('../res/textures/skybox_new/corona_lf.png');
 
     //Add all textures to array as materials
     materialArray.push(new THREE.MeshBasicMaterial({map: texture_ft, side: THREE.DoubleSide}));
